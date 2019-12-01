c1085a7802114c2bb39a748a43d052fa975d2210
Håvar Nøvik <havar.novik@gmail.com>
2019-12-01 12:08:47 +0100

    Added benchmark comparison for bool parser.

--- benchmarks/Program.fs
+++ benchmarks/Program.fs
@@ -43,6 +43,41 @@ type JsonComparison () =
         let result = System.Text.Json.JsonDocument.Parse(inputAsString)
         ()
 
+[<MemoryDiagnoser>]
+type BoolAltComparison () =
+    
+    let mutable inputAsMemory: ReadOnlyMemory<char> = ReadOnlyMemory.Empty
+    let mutable inputAsString: string = String.Empty
+
+    let fparseccTrueParser = stringReturn "true"  (true)
+    let fparseccFalseParser = stringReturn "false" (false)
+    let fparseccBoolParser = FParsec.Primitives.choice [ fparseccTrueParser; fparseccFalseParser; ]
+
+    let nomTrueParser = NomFs.Combinator.map (NomFs.Bytes.Complete.tag (m "true")) (fun _ -> true);
+    let nomFalseParser = NomFs.Combinator.map (NomFs.Bytes.Complete.tag (m "false")) (fun _ -> true);
+    let nomBoolParser = NomFs.Branch.alt [ nomTrueParser; nomFalseParser; ]
+
+    [<Params ("true", "false", "somethingelse")>] 
+    member val public Input = String.Empty with get, set
+
+    [<GlobalSetup>]
+    member self.GlobalSetupData() =
+        inputAsMemory <- (self.Input.AsMemory())
+        inputAsString <- self.Input
+        ()
+
+    [<Benchmark>]
+    member self.NomFsMemoryReadOnly() =
+        match nomBoolParser inputAsMemory with
+        | Ok (_, r) -> Ok r
+        | Error (Err (e, _)) -> Error e
+        | _ -> raise (Exception "should not happen.")
+
+    [<Benchmark>]
+    member self.FparsecPstring() =
+        match run fparseccBoolParser inputAsString with
+        | ParserResult.Success (s, _, _) -> Ok s
+        | ParserResult.Failure (s, _, _) -> Error s
 
 [<MemoryDiagnoser>]
 type TagComparison () =
@@ -82,6 +117,7 @@ let main argv =
     BenchmarkSwitcher.FromTypes([|
         typeof<TagComparison>;
         typeof<JsonComparison>;
+        typeof<BoolAltComparison>;
         |])
         .Run(argv, o) |> ignore
     0 // return an integer exit code
